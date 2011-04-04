481236d11120dcb5ee674d159fcab36d128db6e5
James Frost <james@hydrogen.(none)>
2011-04-04 16:27:32 +0100

    Made plugin implement interface rather than being annotated

--- views/src/morph/plugin/ViewsPlugin.groovy
+++ views/src/morph/plugin/ViewsPlugin.groovy
@@ -1,8 +1,22 @@
 package morph.plugin
 
-import morph.annotations.Plugin
+import morph.plugins.Plugin
 
+class ViewsPlugin implements Plugin {
 
-@Plugin(name="Views", version="1.0.4", description="Provides Groovy Views")
-class ViewsPlugin {
+	@Override
+	String description() {
+		"Provides Groovy Views for Morph"
+	}
+
+	@Override
+	public String name() {
+		"Views"
+	}
+
+	@Override
+	public String version() {
+		"0.4.10"
+	}
+		
 }
