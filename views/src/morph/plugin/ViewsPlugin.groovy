b6c41affbfd5b377cfceddf4c2ab0db5c1b92857
James Frost <james@silver.(none)>
2012-04-09 21:40:39 +0100

    Switched to simpler MavenCentral and added makings of nice XML views

--- views/src/morph/plugin/ViewsPlugin.groovy
+++ views/src/morph/plugin/ViewsPlugin.groovy
@@ -1,7 +1,16 @@
 package morph.plugin
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.RequestToViewNameTranslator;
+import org.springframework.web.servlet.ViewResolver;
+
+import morph.plugin.views.MorphRequestToViewNameTranslator
+import morph.plugin.views.MorphViewResolver
+import morph.plugin.views.annotation.AnnotationMethodHandlerAdapter;
 import morph.plugins.Plugin
 
+@Configuration
 class ViewsPlugin implements Plugin {
 
 	@Override
@@ -18,5 +27,21 @@ class ViewsPlugin implements Plugin {
 	public String version() {
 		"0.4.10"
 	}
-		
+
+	@Bean
+	ViewResolver morphViewResolver() {
+		def resolver = new MorphViewResolver()
+		resolver.cache = false
+		resolver
+	}
+
+	@Bean
+	RequestToViewNameTranslator viewNameTranslator() {
+		new MorphRequestToViewNameTranslator()
+	}
+	
+	@Bean
+	AnnotationMethodHandlerAdapter annotationMethodHandlerAdapter() {
+		new AnnotationMethodHandlerAdapter()
+	}
 }
